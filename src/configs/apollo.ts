cf09b4bb0620e0a6f9d932fe0c91a80558eb3388
shreeyash07 <shresthashree0807@gmail.com>
2024-10-22 11:55:45 +0545

    Add graphql and apollo configuration

---
+++ src/configs/apollo.ts
@@ -0,0 +1,31 @@
+import {
+    ApolloClientOptions,
+    HttpLink,
+    InMemoryCache,
+    NormalizedCacheObject,
+} from '@apollo/client';
+
+const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT as string;
+
+const link = new HttpLink({
+    uri: GRAPHQL_ENDPOINT,
+    credentials: 'include',
+});
+
+const apolloOptions: ApolloClientOptions<NormalizedCacheObject> = {
+    link,
+    cache: new InMemoryCache(),
+    defaultOptions: {
+        watchQuery: {
+            fetchPolicy: 'network-only',
+            nextFetchPolicy: 'cache-only',
+            errorPolicy: 'all',
+        },
+        query: {
+            fetchPolicy: 'network-only',
+            errorPolicy: 'all',
+        },
+    },
+};
+
+export default apolloOptions;
