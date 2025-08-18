3df890f9c61dd5224a14cfa4b897caae43986e29
Shreeyash Shrestha <shreeyash.shrestha@togglecorp.com>
2025-08-18 16:14:01 +0545

    feat(web-app-serve): configure web app serve

--- env.ts
+++ env.ts
@@ -1,12 +1,27 @@
 import {
     defineConfig,
+    overrideDefineForWebAppServe,
     Schema,
 } from '@julr/vite-plugin-validate-env';
 
-export default defineConfig({
-    // NOTE: We need to replace with URL
-    APP_GRAPHQL_ENDPOINT: Schema.string(),
+const webAppServeEnabled = process.env.WEB_APP_SERVE_ENABLED?.toLowerCase() === 'true';
 
-    // NOTE: It is not used for now
-    APP_TITLE: Schema.string(),
+if (webAppServeEnabled) {
+    // eslint-disable-next-line no-console
+    console.warn('Building application for web-app-serve');
+}
+const overrideDefine = webAppServeEnabled
+    ? overrideDefineForWebAppServe
+    : undefined;
+
+export default defineConfig({
+    overrideDefine,
+    validator: 'builtin',
+    schema: {
+        // NOTE: We need to replace with URL
+        APP_GRAPHQL_ENDPOINT: Schema.string(),
+
+        // NOTE: It is not used for now
+        APP_TITLE: Schema.string(),
+    },
 });
